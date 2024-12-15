58066bb9726fabbec2d9a65222940b1fc6868c10
Thomas <thomas@yeoki.be>
2024-12-15 15:45:44 +0100

    build(deps): update 2 files and create 1 file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# dj_polyglot" 
+
+python setup.py sdist bdist_wheel
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dj-polyglot",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/Yeoki-ERP/dj_polyglot",
