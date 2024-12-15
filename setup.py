9dcd06c21687837cec63153011115f8a8992b503
Thomas <thomas@yeoki.be>
2024-12-15 16:08:31 +0100

    build(deps): update publish-to-pypi.yml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dj-polyglot",
-    version="0.3",
+    version="0.4.0",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/Yeoki-ERP/dj_polyglot",
